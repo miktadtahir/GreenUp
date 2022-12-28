02a75306c01dc3b0bea597c6a39f2fb9b5058d9d
miktadtahir <miktadtahirdurak@gmail.com>
2022-12-29 02:33:34 +0300

    Felixart E-commerce

--- chawkbazar-api/resources/views/welcome.blade.php
+++ chawkbazar-api/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Marvel Laravel</title>
+    <title>Felixart E-commerce</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
